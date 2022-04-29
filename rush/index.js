f8baaa026bc7991783cb7791ee8ed9c38efe66fe
Bastien RODRIGUEZ <barodrig@e2r3p15.clusters.42paris.fr>
2022-04-29 18:10:11 +0200

    Added scroll down

--- rush/index.js
+++ rush/index.js
@@ -2,10 +2,12 @@ function p2_trigger()
 {
     document.getElementById('p2').style.display = "block";
     document.getElementById('p3').style.display = "none";
+    document.getElementById('p2').scrollIntoView();
 }
 
 function p3_trigger()
 {
     document.getElementById('p2').style.display = "none";
     document.getElementById('p3').style.display = "block";
+    document.getElementById('p3').scrollIntoView();
 }
