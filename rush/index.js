6219db94ddd2d0b99408a4d4c4e9e21346821f05
Bastien RODRIGUEZ <barodrig@e2r3p15.clusters.42paris.fr>
2022-04-29 18:06:26 +0200

    Fixing js trigger

--- rush/index.js
+++ rush/index.js
@@ -1,11 +1,11 @@
 function p2_trigger()
 {
-    document.getElementById('barodrig').style.display = "block";
-    document.getElementById('clmurphy').style.display = "none";
+    document.getElementById('p2').style.display = "block";
+    document.getElementById('p3').style.display = "none";
 }
 
 function p3_trigger()
 {
-    document.getElementById('barodrig').style.display = "none";
-    document.getElementById('clmurphy').style.display = "block";
+    document.getElementById('p2').style.display = "none";
+    document.getElementById('p3').style.display = "block";
 }
